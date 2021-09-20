b0939056a10659fb0ece1590546833202f80c26d
dxprx <87080137+dxprx@users.noreply.github.com>
2021-09-20 17:50:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,34 +1,29 @@
+
+
 function setup() {
   a = createCanvas(windowWidth, windowHeight, WEBGL);
   noFill();
-  // slider = createSlider(-PI, PI, 0, 0.01);
-  // slider.position(0,475);
-  // // slider.style('width', '500px');
-  // slider.addClass("mySliders");
+
+
+
 }
 
 function draw() {
+  let d = day();
   background(200);
-    //let rad = millis() / 1000;
-  // Set rotation angles
-  //let ct = cos(rad);
-  //let st = sin(rad);
+
   rotateY(PI / 6);
-  // let val = slider.value();
+
   let ct = cos(mouseX/180);
   let st = sin(mouseY/180);
     applyMatrix(  ct, 0.0,  ct,  0.0,
                0.0, 1.0, 0.0,  0.0,
                -st, 0.0,  st,  0.0,
                0.0, 0.0, 0.0,  1.0);
-    //applyMatrix(  -ct, 0.0,  -st,  0.0,
-     //          0.0, 1.0, 0.0,  0.0,
-     //          st, 0.0,  -st,  0.0,
-     //          0.0, 0.0, 0.0,  1.0);
+
   stroke(10);
   box(250);
 
-  // Matrix for rotation around the Y axis
   
   applyMatrix(  ct, 0.0,  -ct,  0.0,
               0.0, 1.0, 0.0,  0.0,
@@ -61,6 +56,6 @@ function draw() {
 
 function mouseClicked(){
   // console.log(mouseX, mouseY);
-  console.log(windowWidth, windowHeight);
+  // console.log(windowWidth, windowHeight);
   
 }
