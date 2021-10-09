eb1cfd1ad544221a6000e2ab8466065064935bd9
oscar <oscargperez00@gmail.com>
2021-10-09 17:57:50 +0200

    Botones fixed and some css
    To do:
    - Mejorar css
    - Maybe cambiar la transformacion del canvas del p5js paraa que no sea tan feo

--- botones.js
+++ botones.js
@@ -1,120 +1,179 @@
-function botones() {
-    var inputReleased = false;
 
-    function setup() {
+var inputReleasedName = false;
+var inputReleasedUrl = false;
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
 
-        let button = createButton('Nuevo URL');
-        button.position(0, 0);
-        button.id('newUrlButton');
+function setup() {
 
-        inp = createInput('');
-        inp.position(0, 0);
-        inp.id('nameNewUrl');
+    let button = createButton('New shortcut');
+    // button.position(window.width-button.width,window.height-button.height);
+    button.id('newUrlButton');
+    // console.log(window.width - (2 * button.width), window.height - (2 * button.height));
+
+    inputName = createInput('');
+    inputUrl = createInput('');
+
+    // inp.position(window.width-inp.width,window.height-inp.height);
+    // console.log(window.width - inp.width, window.height - inp.height);
+
+    inputName.id('nameNewUrl');
+    inputUrl.id('urlNewUrl');
+
+    document.getElementById('nameNewUrl').autocomplete = 'off';
+    document.getElementById('urlNewUrl').autocomplete = 'off';
+
+    document.getElementById('urlNewUrl').type = 'url';
+
+    displayInput();
+    inputName.input(keyTyped);
+    inputName.input(keyPressed);
+    inputUrl.input(keyTyped);
+    inputUrl.input(keyPressed);
+
+    const misBotonesGuardados = getItem('botonesCreados');
+
+    if (misBotonesGuardados !== null) {
+        misBotonesGuardados.forEach(miBotonGuardado => {
+            const btnElement = createButton(miBotonGuardado.text);
+            btnElement.class(miBotonGuardado.className);
+            btnElement.id(miBotonGuardado.id);
+            btnElement.position();
+            document.getElementById(miBotonGuardado.id).setAttribute('onclick', `window.location.href="${miBotonGuardado.url}";`);
+            document.getElementById('flex-container').appendChild(btnElement.elt);
+        });
+    }
+    button.mousePressed(displayButton);
+
+    // console.log(inputReleased);
+}
+
+function draw() {
+    background(220);
+}
+
+function displayButton() {
+    inputReleasedName = true;
+    displayInput();
+    document.getElementById('newUrlButton').style.display = 'none';
+
+    // console.log(inputReleased);
+}
+
+
+function displayInput() {
+
+    if (inputReleasedName === false && inputReleasedUrl === false) {
+        inputName.value('');
+        inputUrl.value('');
+        document.getElementById('nameNewUrl').style.display = 'none';
+        document.getElementById('urlNewUrl').style.display = 'none';
+        document.getElementById('newUrlButton').style.display = '';
+    } else if (inputReleasedName === true && inputReleasedUrl === false) {
+        inputName.value('');
+        document.getElementById('nameNewUrl').style.display = 'block';
+        document.getElementById('nameNewUrl').placeholder = "name";
+        document.getElementById('urlNewUrl').style.display = 'none';
+
+    } else if (inputReleasedName === false && inputReleasedUrl === true) {
+        // inputName.value('');
+        inputUrl.value('');
+        document.getElementById('nameNewUrl').style.display = 'none';
+        document.getElementById('newUrlButton').style.display = 'none';
+        document.getElementById('urlNewUrl').style.display = 'block';
+
+        document.getElementById('urlNewUrl').placeholder = "url";
+    }
+    // console.log(inputReleased);
+
+}
+
+function keyPressed() {
+    if (keyCode === ESCAPE_KEY && inputReleasedName === true) {
+        inputReleasedName = false;
+        inputReleasedUrl = false;
         displayInput();
-        inp.input(keyTyped);
+    } else if (keyCode === ESCAPE_KEY && inputReleasedUrl === true) {
+        inputReleasedName = false;
+        inputReleasedUrl = false;
+        displayInput();
+
+    }
+
+}
+function keyTyped() {
+    //  console.log({
+    //      keyCode,
+    //     // enter: ENTER,
+    //     inputReleased
+    //  });
+    // console.log('you are typing: ', inp.value());
+
+
+
+    if (keyCode === ENTER_KEY && inputReleasedName === true) {
+
+        inputReleasedName = false;
+        inputReleasedUrl = true;
+        displayInput();
+        // console.log(inputReleased);
+
+    } else if (keyCode === ENTER_KEY && inputReleasedUrl === true) {
 
         const misBotonesGuardados = getItem('botonesCreados');
 
-        if (misBotonesGuardados !== null) {
-            misBotonesGuardados.forEach(miBotonGuardado => {
-                const btnElement = createButton(miBotonGuardado.text);
-                btnElement.class(miBotonGuardado.className);
-                btnElement.id(miBotonGuardado.id);
-                btnElement.position();
-            });
+        let newURL = createButton(inputName.value());
+        newURL.position();
+        newURL.class('botonUrl');
+        newURL.id(misBotonesGuardados === null ? 1 : misBotonesGuardados.length + 1);
+
+        const httpwww = 'http://www.';
+        const http = 'http://';
+        const www = 'www.';
+        let inputUrlValue;
+
+        //Chequea que en el inputUrl hayas escrito el http://www o el http://
+        if (inputUrl.value().includes(httpwww)) {
+            document.getElementById(newURL.id()).setAttribute('onclick', `window.location.href="${inputUrl.value()}";`);
+            inputUrlValue = inputUrl.value();
+        } else if (inputUrl.value().includes(www) && !inputUrl.value().includes(http)) {
+            document.getElementById(newURL.id()).setAttribute('onclick', `window.location.href="${http.concat(inputUrl.value())}";`);
+            inputUrlValue = http.concat(inputUrl.value());
+        } else if (!inputUrl.value().includes(www) && !inputUrl.value().includes(http)) {
+            document.getElementById(newURL.id()).setAttribute('onclick', `window.location.href="${httpwww.concat(inputUrl.value())}";`);
+            inputUrlValue = httpwww.concat(inputUrl.value());
         }
-        button.mousePressed(displayButton);
-        
-        // console.log(inputReleased);
-    }
 
-    function draw() {
-        background(220);
-    }
+        const toSave = {
+            id: newURL.id(),
+            className: newURL.class(),
+            url: inputUrlValue,
+            text: newURL.html(),
 
-    function displayButton() {
-        inputReleased = true;
+        }
+        storeItem('botonesCreados', [toSave])
+        if (misBotonesGuardados === null) {
+            storeItem('botonesCreados', [toSave]);
+        } else {
+            storeItem('botonesCreados', [...misBotonesGuardados, toSave]);
+        }
+
+        inputReleasedName = false;
+        inputReleasedUrl = false;
         displayInput();
-        document.getElementById('newUrlButton').style.display = 'none';
 
-        // console.log(inputReleased);
+
+        document.getElementById('flex-container').appendChild(newURL.elt);
     }
-
-
-    function displayInput() {
-
-        if (inputReleased === false) {
-            document.getElementById('nameNewUrl').style.display = 'none';
-        } else if (inputReleased === true) {
-
-            document.getElementById('nameNewUrl').style.display = 'block';
-            
-            
-        }
-        // console.log(inputReleased);
-
-    }
-
-    function keyTyped() {
-        // console.log({
-        //     keyCode,
-        //     // enter: ENTER,
-        //     inputReleased
-        // });
-        // console.log('you are typing: ', inp.value());
-        if (keyCode === 13 && inputReleased === true) {
-
-            const misBotonesGuardados = getItem('botonesCreados');
-
-            newURL = createButton(inp.value());
-            newURL.position();
-            newURL.class('botonUrl');
-            newURL.id(misBotonesGuardados === null ? 1 : misBotonesGuardados.length + 1);
-
-            console.log(newURL);
-
-            const toSave = {
-                id: newURL.id(),
-                className: newURL.class(),
-                text: newURL.html()
-            }
-
-            console.log(toSave);
-
-
-            if (misBotonesGuardados === null) {
-                storeItem('botonesCreados', [toSave]);
-            } else {
-                storeItem('botonesCreados', [...misBotonesGuardados, toSave]);
-            }
-
-
-
-            inputReleased = false;
-
-            document.getElementById('nameNewUrl').style.display = 'none';
-
-            document.getElementById('newUrlButton').style.display = '';
-
-            inp.value('');
-
-            // console.log(inputReleased);
-
-        }
-    }
-    
-
-    // return {
-    //     setup,
-    // }
 }
+
 
 
 /*
 
 TO DO
 
-- Quitar predicciones en el input 
+- Quitar predicciones en el input
 - Poner autofocus al input al clicar boton New URL
 
 
--- sketch.js
+++ sketch.js
@@ -4,25 +4,44 @@
 
 var w = window.innerWidth;
 var h = window.innerHeight;  
-var inputReleased = false;
+var inputReleasedName = false;
+var inputReleasedUrl = false;
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
 
 /*------------------------------------------------------------------------------------------------------------------*/
 // Funciones del P5JS para crear el canvas principal de las figuras
 
 function setup() {
-  a = createCanvas(w, h, WEBGL);
+  a = createCanvas(w-70, h-70, WEBGL);
   noFill();
-
+  a.id('canvas-poligonos');
+  
   //Botones.js setup
-  let button = createButton('Nuevo URL');
-  button.position(0, 0);
+  let button = createButton('New shortcut');
+  // button.position(window.width-button.width,window.height-button.height);
   button.id('newUrlButton');
+  // console.log(window.width - (2 * button.width), window.height - (2 * button.height));
 
-  inp = createInput('');
-  inp.position(0, 0);
-  inp.id('nameNewUrl');
+  inputName = createInput('');
+  inputUrl = createInput('');
+
+  // inp.position(window.width-inp.width,window.height-inp.height);
+  // console.log(window.width - inp.width, window.height - inp.height);
+
+  inputName.id('nameNewUrl');
+  inputUrl.id('urlNewUrl');
+
+  document.getElementById('nameNewUrl').autocomplete = 'off';
+  document.getElementById('urlNewUrl').autocomplete = 'off';
+
+  document.getElementById('urlNewUrl').type = 'url';
+
   displayInput();
-  inp.input(keyTyped);
+  inputName.input(keyTyped);
+  inputName.input(keyPressed);
+  inputUrl.input(keyTyped);
+  inputUrl.input(keyPressed);
 
   const misBotonesGuardados = getItem('botonesCreados');
 
@@ -32,9 +51,16 @@ function setup() {
           btnElement.class(miBotonGuardado.className);
           btnElement.id(miBotonGuardado.id);
           btnElement.position();
+          document.getElementById(miBotonGuardado.id).setAttribute('onclick', `window.open("${miBotonGuardado.url}");`);
+          document.getElementById('flex-container').appendChild(btnElement.elt);
       });
   }
   button.mousePressed(displayButton);
+
+
+  document.getElementById('container').appendChild(document.getElementById('canvas-poligonos'));
+  document.getElementById('container').appendChild(document.getElementById('flex-container'));
+  // console.log(inputReleased);
 }
 
 function draw() {
@@ -88,7 +114,7 @@ function draw() {
 /*------------------------------------------------------------------------------------------------------------------*/
 // Funciones de botones.js
 function displayButton() {
-  inputReleased = true;
+  inputReleasedName = true;
   displayInput();
   document.getElementById('newUrlButton').style.display = 'none';
 
@@ -98,63 +124,107 @@ function displayButton() {
 
 function displayInput() {
 
-  if (inputReleased === false) {
+  if (inputReleasedName === false && inputReleasedUrl === false) {
+      inputName.value('');
+      inputUrl.value('');
       document.getElementById('nameNewUrl').style.display = 'none';
-  } else if (inputReleased === true) {
-
+      document.getElementById('urlNewUrl').style.display = 'none';
+      document.getElementById('newUrlButton').style.display = '';
+  } else if (inputReleasedName === true && inputReleasedUrl === false) {
+      inputName.value('');
       document.getElementById('nameNewUrl').style.display = 'block';
-      
-      
+      document.getElementById('nameNewUrl').placeholder = "name";
+      document.getElementById('urlNewUrl').style.display = 'none';
+
+  } else if (inputReleasedName === false && inputReleasedUrl === true) {
+      // inputName.value('');
+      inputUrl.value('');
+      document.getElementById('nameNewUrl').style.display = 'none';
+      document.getElementById('newUrlButton').style.display = 'none';
+      document.getElementById('urlNewUrl').style.display = 'block';
+
+      document.getElementById('urlNewUrl').placeholder = "url";
   }
   // console.log(inputReleased);
 
 }
 
+function keyPressed() {
+  if (keyCode === ESCAPE_KEY && inputReleasedName === true) {
+      inputReleasedName = false;
+      inputReleasedUrl = false;
+      displayInput();
+  } else if (keyCode === ESCAPE_KEY && inputReleasedUrl === true) {
+      inputReleasedName = false;
+      inputReleasedUrl = false;
+      displayInput();
+
+  }
+
+}
 function keyTyped() {
-  // console.log({
-  //     keyCode,
+  //  console.log({
+  //      keyCode,
   //     // enter: ENTER,
   //     inputReleased
-  // });
+  //  });
   // console.log('you are typing: ', inp.value());
-  if (keyCode === 13 && inputReleased === true) {
+
+
+
+  if (keyCode === ENTER_KEY && inputReleasedName === true) {
+
+      inputReleasedName = false;
+      inputReleasedUrl = true;
+      displayInput();
+      // console.log(inputReleased);
+
+  } else if (keyCode === ENTER_KEY && inputReleasedUrl === true) {
 
       const misBotonesGuardados = getItem('botonesCreados');
 
-      newURL = createButton(inp.value());
+      let newURL = createButton(inputName.value());
       newURL.position();
       newURL.class('botonUrl');
       newURL.id(misBotonesGuardados === null ? 1 : misBotonesGuardados.length + 1);
 
-      console.log(newURL);
+      const httpwww = 'http://www.';
+      const http = 'http://';
+      const www = 'www.';
+      let inputUrlValue;
+
+      //Chequea que en el inputUrl hayas escrito el http://www o el http://
+      if (inputUrl.value().includes(httpwww)) {
+          document.getElementById(newURL.id()).setAttribute('onclick', `window.open("${inputUrl.value()}");`);
+          inputUrlValue = inputUrl.value();
+      } else if (inputUrl.value().includes(www) && !inputUrl.value().includes(http)) {
+          document.getElementById(newURL.id()).setAttribute('onclick', `window.open("${http.concat(inputUrl.value())}");`);
+          inputUrlValue = http.concat(inputUrl.value());
+      } else if (!inputUrl.value().includes(www) && !inputUrl.value().includes(http)) {
+          document.getElementById(newURL.id()).setAttribute('onclick', `window.open("${httpwww.concat(inputUrl.value())}");`);
+          inputUrlValue = httpwww.concat(inputUrl.value());
+      }
 
       const toSave = {
           id: newURL.id(),
           className: newURL.class(),
-          text: newURL.html()
+          url: inputUrlValue,
+          text: newURL.html(),
+
       }
-
-      console.log(toSave);
-
-
+      storeItem('botonesCreados', [toSave])
       if (misBotonesGuardados === null) {
           storeItem('botonesCreados', [toSave]);
       } else {
           storeItem('botonesCreados', [...misBotonesGuardados, toSave]);
       }
 
+      inputReleasedName = false;
+      inputReleasedUrl = false;
+      displayInput();
 
 
-      inputReleased = false;
-
-      document.getElementById('nameNewUrl').style.display = 'none';
-
-      document.getElementById('newUrlButton').style.display = '';
-
-      inp.value('');
-
-      // console.log(inputReleased);
-
+      document.getElementById('flex-container').appendChild(newURL.elt);
   }
 }
 
@@ -210,20 +280,20 @@ startTime();
 
 
 /*------------------------------------------------------------------------------------------------------------------*/
-//  Eventos de raton
+//  Eventos de raton  // Efecto TV
 
-function mousePressed(){
-  window.tv();
+// function mousePressed(){
+//   window.tv();
 
-  // console.log(mouseX, mouseY);
-  // console.log(windowWidth, windowHeight);
+//   // console.log(mouseX, mouseY);
+//   // console.log(windowWidth, windowHeight);
   
-}
+// }
 
-function mouseReleased(){
-  document.getElementById('tv').style.display = 'none';
-  // window.clearTv();
-}
+// function mouseReleased(){
+//   document.getElementById('tv').style.display = 'none';
+//   // window.clearTv();
+// }
 
 /*------------------------------------------------------------------------------------------------------------------*/
 
