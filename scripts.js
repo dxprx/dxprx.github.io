ff8ef667dde0536c33e802e470610c31539e7d30
oscar <oscargperez00@gmail.com>
2023-06-09 16:06:33 +0200

    Beginning

---
+++ scripts.js
@@ -0,0 +1,31 @@
+$(function() {
+  $.scrollify({
+    section: ".section",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset: 0,
+    scrollbars: false,
+    setHeights: false,
+    updateHash: false,
+    before: function(index) {
+      // Opcional: Realizar acciones antes de cambiar de sección
+    },
+    after: function(index) {
+      // Opcional: Realizar acciones después de cambiar de sección
+      updateIndicator(index);
+    }
+  });
+
+  function updateIndicator(index) {
+    var indicator = $('.indicator');
+    var sections = $('.section');
+    var currentSection = sections.eq(index);
+
+    var topOffset = currentSection.offset().top;
+    var sectionHeight = currentSection.outerHeight();
+
+    var indicatorPosition = topOffset + sectionHeight / 2;
+
+    indicator.css('top', indicatorPosition);
+  }
+});
